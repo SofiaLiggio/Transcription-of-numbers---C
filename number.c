79ec6150791776e2a8aee8de8c91c3b2ebc3e099
sofia_liggio <sofialiggio994@gmail.com>
2024-11-11 18:23:59 +0100

    realized first part of project

---
+++ number.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main() {
+
+    int number;
+
+do
+{
+    printf("Write a number between 1 and 10: ");
+    scanf("%d", &number);
+
+    switch (number) {
+    case 1:
+        printf("\nONE\n");
+        break;
+    case 2:
+        printf("\nTWO\n");
+        break;
+    case 3:
+        printf("\nTHREE\n");
+        break;
+    case 4:
+        printf("\nFOUR\n");
+        break;
+    case 5:
+        printf("\nFIVE\n");
+        break;
+    case 6:
+        printf("\nSIX\n");
+        break;
+    case 7:
+        printf("\nSEVEN\n");
+        break;
+    case 8:
+        printf("\nEIGHT\n");
+        break;
+    case 9:
+        printf("\nNINE\n");
+        break;
+    case 10:
+        printf("\nTEN\n");
+        break;
+    
+    default:
+        printf("\n***Number not included***\n\n");
+        break;
+    }
+} while (number <1 || number>10);
+    return 0;
+}
